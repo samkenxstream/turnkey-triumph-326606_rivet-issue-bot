9b0313420489b20d3d26ed97d482d65845d5052d
Scott Anthony Murray <scanmurr@iu.edu>
2019-06-20 10:50:43 -0400

    Stubbed out analytics file

---
+++ webhooks/schedule.repository/analytics.js
@@ -0,0 +1,10 @@
+/** 
+ * Copyright (C) 2019 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ * Sends a report containing some useful metrics related to issues.
+ */
+
+module.exports = async context => {
+  
+}
